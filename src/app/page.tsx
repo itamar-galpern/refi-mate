aaca27cd2fd757922eae98186c93df98567ae413
george <george@cs.huji.ac.il>
2025-08-30 15:10:43 +0300

    Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Say Hello{" "}
+            Say{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
               src/app/page.tsx
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Save and see your changes i have SAVED.
           </li>
         </ol>
 
